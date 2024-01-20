d4a08634a22f5ed460910c16ad0c311e74779e0f
Overstock <44179934+Overstockk@users.noreply.github.com>
2024-01-20 02:40:22 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,110 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let bodyLockStatus = true;
+    let bodyLockToggle = (delay = 500) => {
+        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+    };
+    let bodyUnlock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
+                }
+                body.style.paddingRight = "0px";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            }
+            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                bodyLockToggle();
+                document.documentElement.classList.toggle("menu-open");
+            }
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    var script_image = new Image;
+    script_image.src = "img/main-section/rst/rst.jpg";
+    var overlay = document.createElement("div");
+    overlay.id = "overlay";
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.zIndex = "9999";
+    overlay.style.display = "none";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    overlay.appendChild(script_image);
+    script_image.style.position = "absolute";
+    script_image.style.top = "50%";
+    script_image.style.left = "50%";
+    script_image.style.transform = "translate(-50%, -50%)";
+    document.body.appendChild(overlay);
+    function toggleOverlay() {
+        if (overlay.style.display == "none") {
+            overlay.style.display = "block";
+            document.body.style.overflow = "hidden";
+        } else {
+            overlay.style.display = "none";
+            document.body.style.overflow = "auto";
+        }
+    }
+    document.addEventListener("keydown", (function(event) {
+        if (event.ctrlKey && event.altKey && event.shiftKey && event.keyCode == 115) toggleOverlay();
+    }));
+    overlay.addEventListener("click", (function(event) {
+        toggleOverlay();
+    }));
+    document.addEventListener("keyup", (function(event) {
+        if (event.keyCode == 27) toggleOverlay();
+    }));
+    window["FLS"] = true;
+    isWebp();
+    menuInit();
+})();
